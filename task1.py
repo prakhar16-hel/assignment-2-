2569d55cf65e2dbd505523475861a9136a137537
prakhar16-hel <prakharsv16@gmail.com>
2025-06-09 22:49:47 +0530

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,5 +1,5 @@
-# odd_even  = int(input("enter your no:"))
-# if odd_even % 2 == 0 :
-#     print("your no os even:",odd_even)
-# else:
-#     print("your no is odd",odd_even)
+odd_even  = int(input("enter your no:"))
+if odd_even % 2 == 0 :
+     print("your no os even:",odd_even)
+else:
+     print("your no is odd",odd_even)
